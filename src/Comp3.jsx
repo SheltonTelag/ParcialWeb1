2215a9cd966588661309d5178b934ea53661fa76
Shelton Telag <sheltonstuarttelagvalderrama.97@gmail.com>
2023-08-30 00:05:03 -0500

    cambiando punto 3

--- src/Comp3.jsx
+++ src/Comp3.jsx
@@ -4,37 +4,39 @@ import './app.css'
 const Comp3 = () => {
   const [nombre, setNombre] = useState('')
   const [hora, setHora] = useState('')
+  const [saludos, setSaludos] = useState([])
 
-    const saludo = () => {
-
-    if (hora<0){
-        console.log('Error tiene q poner una hora mayor o igual a 0')
-        setNombre('')
-        setHora('')
-    }else if(hora >= 0 && hora <= 11) {
-        console.log('Buenos días, ' + nombre)
-        setNombre('')
-        setHora('')
-    } else if (hora >= 12 && hora <= 18) {
-        console.log('Buenas tardes, ' + nombre)
-        setNombre('')
-        setHora('')
-    } else if(hora >= 19 && hora <= 23) {
-        console.log('Buenas noches, ' + nombre)
-        setNombre('')
-        setHora('')
-    }else{
-        console.log('Error tiene q poner una hora menor o igual a 23')
-        setNombre('')
-        setHora('')
+  const generarSaludo = () => {
+    if (hora < 0 || hora > 23) {
+      console.log('Error: Por favor, ingrese una hora válida (0-23)')
+      return
     }
+
+    let nuevoSaludo = ''
+
+    if (hora >= 0 && hora <= 11) {
+      nuevoSaludo = 'Buenos días, ' + nombre
+    } else if (hora >= 12 && hora <= 18) {
+      nuevoSaludo = 'Buenas tardes, ' + nombre
+    } else {
+      nuevoSaludo = 'Buenas noches, ' + nombre
+    }
+
+    setSaludos([...saludos, nuevoSaludo])
+    setNombre('')
+    setHora('')
   }
 
-    return <>
-        <h1>Saludo según la Hora</h1>
-        <input type='text' placeholder='Ingrese su nombre' value={nombre} onChange={(e) => setNombre(e.target.value)} />
-        <input type='number' placeholder='Ingrese la hora (0-23)' value={hora} onChange={(e) => setHora(e.target.value)} />
-        <button onClick={saludo}>Obtener Saludo</button>
+  return <>
+      <h1>Saludo según la Hora</h1>
+      <input type='text' placeholder='Ingrese su nombre' value={nombre} onChange={(e) => setNombre(e.target.value)} />
+      <input type='number' placeholder='Ingrese la hora (0-23)' value={hora} onChange={(e) => setHora(e.target.value)} />
+      <button onClick={generarSaludo}>Obtener Saludo</button>
+      <ul>
+        {saludos.map((saludo, index) => (
+          <li key={index}>{saludo}</li>
+        ))}
+      </ul>
     </>
 }
 
